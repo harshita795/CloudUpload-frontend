143885d7e9f8176456762b38e38a3c07dbfefddb
harshita795 <yadavharshita261303@gmail.com>
2025-02-15 19:01:02 +0530

    feat: Renamed app.js to App.js to fix case sensitivity

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { JWT_TOKEN } from './constants/imageUpload';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./app";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { JWT_TOKEN } from "./constants/imageUpload";
 
-const jwtTOKEN = JWT_TOKEN; 
-localStorage.setItem('jwtToken',jwtTOKEN);
+const jwtTOKEN = JWT_TOKEN;
+localStorage.setItem("jwtToken", jwtTOKEN);
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
